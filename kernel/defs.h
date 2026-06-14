c7514bf482e8eae02b58abfcbfdd1a29cb2c30de
Tevel Schlezinger <tevelschlez@gmail.com>
2026-06-14 20:09:23 +0000

    implemented the reset to kernel fb function in order to prevent race condition

--- kernel/defs.h
+++ kernel/defs.h
@@ -190,6 +190,7 @@ void            virtio_disk_intr(void);
 void            virtio_gpu_init(void);
 void            virtio_gpu_commit(void);
 void            display_daemon(void);
+void            virtio_gpu_reset_to_kernel_fb(void);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
