69e54da7b73e8ba1f2040382609467b3eb8adabf
Tevel Schlezinger <tevelschlez@gmail.com>
2026-06-14 20:14:18 +0000

    added the flip function to defs

--- kernel/defs.h
+++ kernel/defs.h
@@ -191,6 +191,7 @@ void            virtio_gpu_init(void);
 void            virtio_gpu_commit(void);
 void            display_daemon(void);
 void            virtio_gpu_reset_to_kernel_fb(void);
+int             virtio_gpu_flip(pagetable_t, uint64);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
